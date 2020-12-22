3b66416beb7a2d3b51a5a5b2a649e4142feadf3b
Yoon Seong Bin <44089734+yansigit@users.noreply.github.com>
2020-12-22 23:41:17 +0900

    update

--- leetcode/217.go
+++ leetcode/217.go
@@ -1,39 +1,62 @@
-//Given an array of integers, find if the array contains any duplicates.
+//Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 //
-//Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+//Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
 
 //Example 1:
 //
-//Input: [1,2,3,1]
-//Output: true
+//Input: nums = [3,0,1]
+//Output: 2
+//Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
 //Example 2:
 //
-//Input: [1,2,3,4]
-//Output: false
+//Input: nums = [0,1]
+//Output: 2
+//Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
 //Example 3:
 //
-//Input: [1,1,1,3,3,4,3,2,4,2]
-//Output: true
+//Input: nums = [9,6,4,2,3,5,7,0,1]
+//Output: 8
+//Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+//Example 4:
+//
+//Input: nums = [0]
+//Output: 1
+//Explanation: n = 1 since there is 1 number, so all numbers are in the range [0,1]. 1 is the missing number in the range since it does not appear in nums.
 
 package main
 
 import "sort"
 
 func main() {
-	print(containsDuplicate([]int{1, 2, 3, 4, 5, 5}))
+	print(missingNumber([]int{0, 1, 2}))
 }
 
-func containsDuplicate(nums []int) bool {
+func missingNumber(nums []int) int {
 	sort.Slice(nums, func(i, j int) bool {
 		return nums[i] < nums[j]
 	})
+
+	if nums[0] != 0 {
+		return 0
+	}
+
+	if len(nums) == 1 {
+		if nums[0] == 0 {
+			return 1
+		} else if nums[0] == 1 {
+			return 0
+		}
+	}
+
 	for i, v := range nums {
 		if i == len(nums)-1 {
 			break
 		}
-		if v == nums[i+1] {
-			return true
+		if v != nums[i+1]-1 {
+			// println(v, nums[i+1]-1)
+			return nums[i+1] - 1
 		}
 	}
-	return false
+
+	return nums[len(nums)-1] + 1
 }
