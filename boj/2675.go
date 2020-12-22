18fb413e30097f79a5802dc5dbb7026bb95d8158
Yoon Seong Bin <44089734+yansigit@users.noreply.github.com>
2020-12-22 22:47:57 +0900

    update

--- boj/2675.go
+++ boj/2675.go
@@ -14,12 +14,12 @@ func main() {
 
 	r := bufio.NewReader(os.Stdin)
 	w := bufio.NewWriter(os.Stdout)
-	for i:=0; i<T; i++ {
-		s,_ := r.ReadString('\n')
+	for i := 0; i < T; i++ {
+		s, _ := r.ReadString('\n')
 		a := strings.Fields(s)
 
-		c,_ := strconv.Atoi(a[0])
-		for _,e := range a[1] {
+		c, _ := strconv.Atoi(a[0])
+		for _, e := range a[1] {
 			for j := 0; j < c; j++ {
 				fmt.Fprint(w, string(e))
 			}
