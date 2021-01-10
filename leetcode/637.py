4e36c1e423f6c0583ef7027016ffdade029a5ec3
Yoon Seong Bin <44089734+yansigit@users.noreply.github.com>
2021-01-10 21:50:27 +0900

    update

---
+++ leetcode/637.py
@@ -0,0 +1,28 @@
+from collections import deque
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def averageOfLevels(self, root: TreeNode) -> List[float]:
+        queue = deque([root])
+        rtn: List[float] = []
+
+        while queue:
+            level_nodes = [node.val for node in queue]
+            rtn.append(sum(level_nodes) / len(level_nodes))
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+        return rtn
